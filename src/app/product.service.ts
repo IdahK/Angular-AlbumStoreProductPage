5ee1b3aea040b9fb8eae2c97f8d910e81136067d
IdahK <idah.koome@strathmore.edu>
2020-10-14 23:04:22 +0300

    Setup Product Service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import  {Album} from './album';
+import {Product} from './product';
+
+// import { Album } from './album';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  private _albumUrl = "../assets/album.json";
+  private _productsUrl = "../assets/products.json";
+  // _http: Http;
+  
+  constructor(private _http:Http) {
+    // this._http = _http;
+   }
 
+
+  getAlbum(id:number): Observable<Album> {
+
+    return this._http.get(this._albumUrl).map((response) =><Album> response.json());
+
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response)=><Product[]>response.json());
+  }
 }
